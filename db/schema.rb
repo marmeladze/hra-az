a413904893f63abaae7edd24c2cf147ea5604558
ziya <ziya.bayramov.1@yandex.com>
2017-07-09 14:13:04 +0400

    Add home

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170709092143) do
+ActiveRecord::Schema.define(version: 20170709094203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
