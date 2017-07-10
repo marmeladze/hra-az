70cc17ab0440482645825797bda17f1a3ffe511f
ziya <ziya.bayramov.1@yandex.com>
2017-07-10 10:24:00 +0400

    Fix questions error

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,10 @@ class Question < ActiveRecord::Base
   after_save :update_slug
 
   validates :title, :answer, presence: true
+
+  def anchorize
+    "#"+slug
+  end
   
   def update_slug
     self.update_column(:slug, self.title.gsub('ə', 'e').parameterize)
