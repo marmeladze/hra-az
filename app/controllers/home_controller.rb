d95ddd40497a65314568868fb01bcfa1bd378ea8
ziya <ziya.bayramov.1@yandex.com>
2017-07-24 13:00:11 +0400

    remove programmes from featured

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @active_pr = programmes.first
     @rest_pr = programmes - [@active_pr]
     cat = Category.find_by(slug: 'xeberler')
-    @featured = Article.where.not(category_id: cat.id).limit(3)
+    @featured = Article.where.not(category_id: [cat.id, 5]).limit(3)
     articles = cat.articles.for_slider
     @active = articles.first
     @rest = articles - [@active]
